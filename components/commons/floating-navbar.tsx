ac684b49a6cbf174d7e0b2dc82f72311a3baf074
Ruby Izekor <1izekorruby@gmail.com>
2025-03-13 07:02:39 +0100

    Update floating-navbar.tsx

--- components/commons/floating-navbar.tsx
+++ components/commons/floating-navbar.tsx
@@ -18,7 +18,7 @@ export const FloatingNav = ({
   navItems: {
     name: string;
     link: string;
-    icon?: JSX.Element;
+    icon?: React.ReactElement;
     disabled?: boolean; // Add disabled prop
   }[];
   className?: string;
